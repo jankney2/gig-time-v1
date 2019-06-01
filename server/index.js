63b82c23a6ea567aca07ebb1d94a390a0d0f23cd
Jacob Ankney <Jacob@Jacobs-MBP.app.byu.edu>
2019-06-01 12:49:45 -0600

    endpoints added, but not flushed out

--- server/index.js
+++ server/index.js
@@ -26,5 +26,36 @@ massive(CONNECTION_STRING).then( (database)=> {
   })
 })
 
+//endpoints (not done)
 
+
+// app.get('/users/:id', userCtrl.getUser)
+// app.post('/users/create', userCtrl.createUser)
+// app.put('/users/edit/:id', userCtrl.editUser)
+// app.delete('/users/delete/:id', userCtrl.deleteUser)
+
+// app.get('/clients/:id', clientCtrl.getClient)
+// app.post('/clients/create/:userId', clientCtrl.createClient)
+// app.put('/clients/edit/:id', clientCtrl.editClient)
+// app.delete('/clients/delete/:id', clientCtrl.deleteClient)
+
+// //gets all gigs for a user
+// app.get('/gigs/:userId', gigCtrl.getUserGigs)
+// //gets only one gig
+// app.get('/gigs/:gigId', gigCtrl.getIndividualGig)
+// app.post('/gigs/create/:userId', gigCtrl.createGig)
+// app.put('/gigs/edit/:gigId', gigCtrl.editGig)
+// app.delete('/gigs/delete/:id', gigCtrl.deleteGig)
+
+
+// app.get('/tasks/:id', taskCtrl.getTask)
+// app.post('/tasks/create/:userId', taskCtrl.createTask)
+// app.put('/tasks/edit/:id', taskCtrl.editTask)
+// app.delete('/tasks/delete/:id', taskCtrl.deleteTask)
+
+
+//bill entire gig
+// app.post('/billing/gig/:gigId', billingCtrl.billGig)
+//bill individual task
+// app.post('/billing/task/:taskId', billingCtrl.billTask)
 
